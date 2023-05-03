50000fe600d472c708d249804f2fd7546befdab1
AyahDweikat <dweikatayah99@gmail.com>
2023-05-03 14:38:09 +0300

    Contact section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import About from './Components/About/About';
 import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
 import Projects from './Components/Projects/Projects';
+import Contact from './Components/Conatct/Contact';
 
 import './responsive.css'
 
@@ -12,6 +13,7 @@ function App() {
       <Home />
       <About />
       <Projects />
+      <Contact />
     </div>
   );
 }
