e9ccc30d152078b822c85f0509634a2fa1cfe20f
AyahDweikat <dweikatayah99@gmail.com>
2023-05-03 10:43:36 +0300

    Hero Section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
 import Header from './Components/Header/Header';
-
+import Home from './Components/Home/Home';
+import './responsive.css'
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
+      <Home />
     </div>
   );
 }
